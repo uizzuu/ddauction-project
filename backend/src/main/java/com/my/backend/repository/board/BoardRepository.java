88a96d054a15ddcfe2b5ea836596f7f2c3cbfd01
ravvitkim <mirkim6086@naver.com>
2025-10-16 14:42:29 +0900

    게시판 리포지토리 추가

---
+++ backend/src/main/java/com/my/backend/repository/board/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.my.backend.repository.board;
+
+import com.my.backend.entity.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
---
+++ backend/src/main/java/com/my/backend/repository/board/CommentRepository.java
@@ -0,0 +1,18 @@
+package com.my.backend.repository.board;
+
+import com.my.backend.entity.User;
+import com.my.backend.entity.board.Article;
+import com.my.backend.entity.board.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 특정 Article의 댓글 리스트 조회
+    List<Comment> findByArticle(Article article);
+
+    // User별 댓글 조회
+    List<Comment> findByUser(User user);
+}
