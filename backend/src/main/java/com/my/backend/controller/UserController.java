d9ecbbf962676bd4495201c56227bb960ec7561b
mayjoonll <mayjoonll@gmail.com>
2025-10-17 15:55:41 +0900

    Reapply "user dto password 추가"
    This reverts commit 760d81451b20db6321af11dad41364679afc7759.

--- backend/src/main/java/com/my/backend/controller/UserController.java
+++ backend/src/main/java/com/my/backend/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.my.backend.controller;
 
-import com.my.backend.dto.UserUpdateDto;
-import com.my.backend.entity.User;
+import com.my.backend.dto.UserDto;
 import com.my.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,23 +15,24 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
+    public UserDto getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public UserDto createUser(@RequestBody UserDto dto) {
+        return userService.createUser(dto);
     }
 
-    @PutMapping("/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
-        return userService.updateUser(id, user);
+    // PUT: PathVariable 제거, DTO에서 id 사용
+    @PutMapping
+    public UserDto updateUser(@RequestBody UserDto dto) {
+        return userService.updateUser(dto);
     }
 
     @DeleteMapping("/{id}")
@@ -40,17 +40,14 @@ public class UserController {
         userService.deleteUser(id);
     }
 
-    @PutMapping("/{id}/mypage")
-    public User updateMyPage(
-            @PathVariable Long id,
-            @RequestBody UserUpdateDto dto
-    ) {
-        return userService.updateUserInfo(id, dto);
+    // 마이페이지 업데이트도 동일하게 DTO 사용
+    @PutMapping("/mypage")
+    public UserDto updateMyPage(@RequestBody UserDto dto) {
+        return userService.updateUser(dto);
     }
 
     @GetMapping("/{id}/mypage")
-    public User getMyPage(@PathVariable Long id) {
+    public UserDto getMyPage(@PathVariable Long id) {
         return userService.getUser(id);
     }
-
 }
