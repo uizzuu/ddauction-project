32a9b4d6f82456b2d505ba89ba6d9d6199c3b86a
mayjoonll <mayjoonll@gmail.com>
2025-10-17 15:54:39 +0900

    user dto password 추가
    # Conflicts:
    #	backend/src/main/java/com/my/backend/controller/UserController.java

--- backend/src/main/java/com/my/backend/controller/UserController.java
+++ backend/src/main/java/com/my/backend/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.my.backend.controller;
 
-import com.my.backend.dto.UserDto;
+import com.my.backend.dto.UserUpdateDto;
 import com.my.backend.entity.User;
 import com.my.backend.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,9 +26,8 @@ public class UserController {
     }
 
     @PostMapping
-    public UserDto createUser(@RequestBody @Valid UserDto userDto) {
-        User user = userService.createUser(userDto.toEntity());
-        return UserDto.fromEntity(user);
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
     }
 
     @PutMapping("/{id}")
@@ -42,5 +40,17 @@ public class UserController {
         userService.deleteUser(id);
     }
 
+    @PutMapping("/{id}/mypage")
+    public User updateMyPage(
+            @PathVariable Long id,
+            @RequestBody UserUpdateDto dto
+    ) {
+        return userService.updateUserInfo(id, dto);
+    }
+
+    @GetMapping("/{id}/mypage")
+    public User getMyPage(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
 
 }
