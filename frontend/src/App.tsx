26cf6ea489d99eb0edbfa9363583384dcce71b9f
ravvitkim <mirkim6086@naver.com>
2025-10-21 16:36:39 +0900

    게시판관련라우트추가함

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useState } from "react";
+import ArticleForm from "./pages/ArticleForm";
+import ArticleDetail from "./pages/ArticleDetail";
+import ArticleList from "./pages/ArticleList";
 import {
   HeaderMain,
   HeaderSub,
@@ -13,7 +16,7 @@ import {
 } from "./import/import";
 import "./import/import.css";
 import type { User, Category } from "./types/types";
-import ArticleList from "./pages/ArticleList";
+
 
 
 export default function App() {
@@ -27,16 +30,19 @@ export default function App() {
   return (
     <div style={{ minHeight: "100vh" }}>
       {showHeader && <HeaderMain user={user} setUser={setUser} />}
-      {showHeader && <HeaderSub category={category} setCategory={setCategory}/>}
+      {showHeader && <HeaderSub category={category} setCategory={setCategory} />}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/auction" element={<ProductList />} />
         <Route path="/register" element={<ProductRegister user={user} />} />
-        <Route path="/mypage" element={<MyPage user={user} setUser={setUser}/>} />
+        <Route path="/mypage" element={<MyPage user={user} setUser={setUser} />} />
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/community" element={<ArticleList user={user} />} />
+        <Route path="/articles/new" element={<ArticleForm userId={user?.userId ?? null} />} />
+        <Route path="/articles/:id/edit" element={<ArticleForm userId={user?.userId ?? null} />} />
+        <Route path="/articles/:id" element={<ArticleDetail user={user} />} />
 
       </Routes>
     </div>
