6538226a00beb4754638b4a73a7d5a699f8f3ec8
ravvitkim <mirkim6086@naver.com>
2025-10-30 15:13:18 +0900

    주소,이미지관련클래스추가

---
+++ backend/src/main/java/com/my/backend/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.my.backend.repository;
+
+import com.my.backend.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
