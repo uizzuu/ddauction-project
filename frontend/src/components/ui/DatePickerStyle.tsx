57f19641ba494927ccb037707d6eab5a6db582bc
uizzuu <psypsy1346@naver.com>
2025-12-12 15:31:59 +0900

    useRef 삭제

--- frontend/src/components/ui/DatePickerStyle.tsx
+++ frontend/src/components/ui/DatePickerStyle.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../../css/datepicker.css";
